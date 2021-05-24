1d9cad0a0c20fcea9c9b1c59b51fd9771856b4b3
mrsameer <sameershaik2001@outlook.com>
2021-05-24 13:27:15 +0530

    Added Student.java

---
+++ src/main/java/com/example/demoProject5/Student.java
@@ -0,0 +1,13 @@
+package com.example.demoProject5;
+
+public class Student {
+    int id;
+    String name;
+    String classroom;
+
+    public Student(int id, String name, String classroom) {
+        this.id = id;
+        this.name = name;
+        this.classroom = classroom;
+    }
+}
